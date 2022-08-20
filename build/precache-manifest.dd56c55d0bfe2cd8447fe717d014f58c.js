49796cac327d113a87fa681f2423acc5a723c853
Dhruv Shah <dhruvshah38@gmail.com>
2022-08-20 14:55:56 -0400

    firebase deploy

---
+++ build/precache-manifest.dd56c55d0bfe2cd8447fe717d014f58c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d61542fb1de6c182f504e504f6fca7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05b20f36e812aeac27e5",
+    "url": "/static/css/2.aff489bc.chunk.css"
+  },
+  {
+    "revision": "afd7fa79da7d71d0894f",
+    "url": "/static/css/main.e52c73d6.chunk.css"
+  },
+  {
+    "revision": "05b20f36e812aeac27e5",
+    "url": "/static/js/2.591c037b.chunk.js"
+  },
+  {
+    "revision": "d1d31d3669c908d912df6a0a7fc1ddf2",
+    "url": "/static/js/2.591c037b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd7fa79da7d71d0894f",
+    "url": "/static/js/main.e7446545.chunk.js"
+  },
+  {
+    "revision": "69bbd7c1381a265f29c9",
+    "url": "/static/js/runtime-main.fda65720.js"
+  }
+]);
